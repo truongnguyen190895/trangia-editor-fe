09fdddc370132768edf327ec76d92b4c5120aaee
Nguyễn Đức Trường <nguyenductruong@Mac.lan>
2025-11-07 01:53:11 +0700

    add loading animation

--- src/pages/document-editor/index.tsx
+++ src/pages/document-editor/index.tsx
@@ -23,9 +23,12 @@ import { getWorkHistoryById } from "@/api/contract";
 import { useEffect } from "react";
 import type { Couple, SingleAgreementParty } from "@/models/agreement-entity";
 import { useThemChuTheContext } from "@/context/them-chu-the";
+import { LoadingDialog } from "@/components/common/loading-dialog";
+import { useState } from "react";
 
 export const DocumentEditor = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
   const [searchParams] = useSearchParams();
   const name = searchParams.get("name");
   const id = searchParams.get("id");
@@ -38,27 +41,32 @@ export const DocumentEditor = () => {
 
   useEffect(() => {
     if (id) {
-      getWorkHistoryById(id).then((res) => {
-        const originalPayload = res.content.original_payload;
-        if (originalPayload) {
-          originalPayload.partyA["cá_nhân"].forEach(
-            (entity: SingleAgreementParty) => {
-              addSinglePartyAEntity(entity);
-            }
-          );
-          originalPayload.partyA["vợ_chồng"].forEach((entity: Couple) => {
-            addCouplePartyAEntity(entity);
-          });
-          originalPayload.partyB["cá_nhân"].forEach(
-            (entity: SingleAgreementParty) => {
-              addSinglePartyBEntity(entity);
-            }
-          );
-          originalPayload.partyB["vợ_chồng"].forEach((entity: Couple) => {
-            addCouplePartyBEntity(entity);
-          });
-        }
-      });
+      setLoading(true);
+      getWorkHistoryById(id)
+        .then((res) => {
+          const originalPayload = res.content.original_payload;
+          if (originalPayload) {
+            originalPayload.partyA["cá_nhân"].forEach(
+              (entity: SingleAgreementParty) => {
+                addSinglePartyAEntity(entity);
+              }
+            );
+            originalPayload.partyA["vợ_chồng"].forEach((entity: Couple) => {
+              addCouplePartyAEntity(entity);
+            });
+            originalPayload.partyB["cá_nhân"].forEach(
+              (entity: SingleAgreementParty) => {
+                addSinglePartyBEntity(entity);
+              }
+            );
+            originalPayload.partyB["vợ_chồng"].forEach((entity: Couple) => {
+              addCouplePartyBEntity(entity);
+            });
+          }
+        })
+        .finally(() => {
+          setLoading(false);
+        });
     }
   }, [id]);
 
@@ -187,6 +195,7 @@ export const DocumentEditor = () => {
         </Button>
         {renderContent()}
       </Box>
+      <LoadingDialog open={loading} />
     </Box>
   );
 };
