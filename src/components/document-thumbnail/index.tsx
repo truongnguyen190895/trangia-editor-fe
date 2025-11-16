071d1f9bbe86275a29ddff95f5b0b24853a94f55
Nguyễn Đức Trường <nguyenductruong@Mac.lan>
2025-11-16 21:06:26 +0700

    add uchi status

--- src/components/document-thumbnail/index.tsx
+++ src/components/document-thumbnail/index.tsx
@@ -1,34 +1,47 @@
 import { Box, Typography } from "@mui/material";
 import AttachFileIcon from "@mui/icons-material/AttachFile";
+import CheckBoxIcon from "@mui/icons-material/CheckBox";
+import RemoveCircleIcon from "@mui/icons-material/RemoveCircle";
 
 interface DocumentThumbnailProps {
   title: string;
+  isUchiReady: boolean;
   onClick: () => void;
 }
 
-export const DocumentThumbnail = ({ title, onClick }: DocumentThumbnailProps) => {
+export const DocumentThumbnail = ({
+  title,
+  isUchiReady,
+  onClick,
+}: DocumentThumbnailProps) => {
   return (
-    <Box
-      display="flex"
-      alignItems="center"
-      gap={1}
-      sx={{
-        cursor: "pointer",
-        border: "1px solid #000",
-        padding: 1,
-        borderRadius: 1,
-        "&:hover": {
-          backgroundColor: "#f5f5f5",
-          transform: "scale(1.01)",
-          transition: "all 0.2s ease-in-out"
-        },
-      }}
-      onClick={onClick}
-    >
-      <AttachFileIcon />
-      <Typography variant="h6" sx={{ fontSize: "14px" }}>
-        {title}
-      </Typography>
+    <Box display="flex" alignItems="center" gap={1}>
+      <Box
+        display="flex"
+        alignItems="center"
+        gap={1}
+        flex={1}
+        sx={{
+          cursor: "pointer",
+          border: "1px solid #000",
+          padding: 1,
+          borderRadius: 1,
+          "&:hover": {
+            backgroundColor: "#f5f5f5",
+            transform: "scale(1.01)",
+            transition: "all 0.2s ease-in-out",
+          },
+        }}
+        onClick={onClick}
+      >
+        <AttachFileIcon />
+        <Typography variant="h6" sx={{ fontSize: "14px" }}>
+          {title}
+        </Typography>
+      </Box>
+      <Box display="flex" alignItems="center" justifyContent="center" gap={1} sx={{ minWidth: "100px" }}>
+        {isUchiReady ? <CheckBoxIcon color="success"/> : <RemoveCircleIcon color="error"/>}
+      </Box>
     </Box>
   );
 };
--- src/pages/documents/index.tsx
+++ src/pages/documents/index.tsx
@@ -16,13 +16,34 @@ export const Documents = () => {
       <Typography fontWeight={600} variant="h3">
         Chọn văn bản cần chỉnh sửa
       </Typography>
-      <Box display="flex" flexDirection="column" gap={2} mt={5}>
+      <Box
+        display="flex"
+        alignItems="center"
+        justifyContent="space-between"
+        gap={2}
+        mt={2}
+        bgcolor="#799EFF"
+        height="70px"
+        px="10px"
+        borderRadius="5px"
+      >
+        <Typography fontWeight={600} variant="h6">
+          Tên mẫu
+        </Typography>
+        <Typography fontWeight={600} variant="h6">
+          Hỗ trợ lên uchi
+        </Typography>
+      </Box>
+      <Box display="flex" flexDirection="column" gap={2} mt={2}>
         {documents.map((document) => (
           <DocumentThumbnail
             key={document.id}
             title={document.name}
+            isUchiReady={document.templateId > 0}
             onClick={() =>
-              navigate(`/editor?type=${document.type}&name=${document.path}&templateId=${document.templateId}`)
+              navigate(
+                `/editor?type=${document.type}&name=${document.path}&templateId=${document.templateId}`
+              )
             }
           />
         ))}
